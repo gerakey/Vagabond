4e558f473d67a980255c023a5b9269cfb996ea42
Cameron White <cameron.white@ufl.edu>
2021-01-12 18:42:23 -0500

    Seperated deployement and build routines

--- build.sh
+++ build.sh
@@ -6,8 +6,4 @@ cd ..
 # Copy compiled client into static folder of server
 cp -dr ./client/build/* ./server/static/
 
-# Deploy app
-sudo rm -dr /var/www/$USER/*
-sudo cp -dr server/* /var/www/$USER/
-
-echo "Build process complete. Application has been deployed. Please restart the apache web server if needed."
+echo "Build process complete."
