3d441f80a2aae6b6a4a8361ec01c96154640afe5
Cameron White <cameron.white@ufl.edu>
2021-01-13 16:04:14 -0500

    Added vagabond config file & project skeleton

---
+++ server/config.py
@@ -0,0 +1,8 @@
+VAGABOND_CONFIG = {
+
+    'mysql_server': '127.0.0.1',
+    'mysql_user': 'cameron',
+    'mysql_password': 'password',
+    'mysql_port': '3306',
+    'mysql_database': 'cameron'
+}
