e993248fc7b3b7e65307970ded7a0a57c241cac1
gerakey <33693472+gerakey@users.noreply.github.com>
2021-03-11 11:47:25 -0500

    Added unique constraint to username and actor (#37)
    Co-authored-by: Gerrad <gerrad@vps-b5288cd9.vps.ovh.ca>

--- server/vagabond/models/User.py
+++ server/vagabond/models/User.py
@@ -4,7 +4,7 @@ from bcrypt import hashpw, gensalt
 
 class User(db.Model):
     id = db.Column(db.Integer, nullable=False, primary_key=True)
-    username = db.Column(db.String(32), nullable=False)
+    username = db.Column(db.String(32), unique = True ,nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
 
     primary_actor_id = db.Column(db.Integer, db.ForeignKey('actor.id'))
